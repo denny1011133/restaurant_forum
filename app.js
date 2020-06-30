a14d044c8890086534873307ead9c602c07573e0
Denny <denny1011133@gmail.com>
2020-06-30 21:31:01 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
