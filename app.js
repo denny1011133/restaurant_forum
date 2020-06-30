c67650568cb1af13d49ad8757e91270d5c0c99f0
Denny <denny1011133@gmail.com>
2020-06-30 20:01:22 +0800

    Create user model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
+const db = require('./models')
 const app = express()
 const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
