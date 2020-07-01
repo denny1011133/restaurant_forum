733855022ee43ff0b6ca2e68c5f1f5ab0099802b
Denny <denny1011133@gmail.com>
2020-07-01 22:57:01 +0800

    Listen to process.env.PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
